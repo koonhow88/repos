cc08562e75b018aa3f4e56984dbbb7c34f7c4432
CHONG KOON HOW <TP041664@mail.apu.edu.my>
2019-11-08 10:59:07 +0800

    Add project files.

---
+++ fordeployment/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace fordeployment.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
